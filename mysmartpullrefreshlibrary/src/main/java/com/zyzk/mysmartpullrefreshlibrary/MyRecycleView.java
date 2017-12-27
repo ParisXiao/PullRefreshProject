9b432e2eb19ed0e2adcbbdc94c3c3ba6b7a4f659
ParisXiao <1451183076@qq.com>
2017-12-27 11:12:35 +0800

    Initial commit

---
+++ mysmartpullrefreshlibrary/src/main/java/com/zyzk/mysmartpullrefreshlibrary/MyRecycleView.java
@@ -0,0 +1,76 @@
+package com.zyzk.mysmartpullrefreshlibrary;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * 具有空布局的RecycleView
+ * Created by Admin on 2017/12/27.
+ */
+
+public class MyRecycleView extends RecyclerView {
+    private View emptyView;
+    private static final String TAG = "MyRecycleView";
+    final private AdapterDataObserver observer = new AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            checkIfEmpty();
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            Log.i(TAG, "onItemRangeInserted" + itemCount);
+            checkIfEmpty();
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            checkIfEmpty();
+        }
+    };
+
+    public MyRecycleView(Context context) {
+        super(context);
+    }
+
+    public MyRecycleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyRecycleView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+    private void checkIfEmpty() {
+        if (emptyView != null && getAdapter() != null) {
+            final boolean emptyViewVisible =
+                    getAdapter().getItemCount() == 0;
+            emptyView.setVisibility(emptyViewVisible ? VISIBLE : GONE);
+            setVisibility(emptyViewVisible ? GONE : VISIBLE);
+        }
+    }
+
+    @Override
+    public void setAdapter(Adapter adapter) {
+        final Adapter oldAdapter = getAdapter();
+        if (oldAdapter != null) {
+            oldAdapter.unregisterAdapterDataObserver(observer);
+        }
+        super.setAdapter(adapter);
+        if (adapter != null) {
+            adapter.registerAdapterDataObserver(observer);
+        }
+
+        checkIfEmpty();
+    }
+
+    //设置没有内容时，提示用户的空布局
+    public void setEmptyView(View emptyView) {
+        this.emptyView = emptyView;
+        checkIfEmpty();
+    }
+
+}
---
+++ mysmartpullrefreshlibrary/src/main/java/com/zyzk/mysmartpullrefreshlibrary/MySmartPullRefresh.java
@@ -0,0 +1,25 @@
+package com.zyzk.mysmartpullrefreshlibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+
+/**
+ * Created by Admin on 2017/12/27.
+ */
+
+public class MySmartPullRefresh extends SmartRefreshLayout {
+    public MySmartPullRefresh(Context context) {
+        super(context);
+    }
+
+    public MySmartPullRefresh(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MySmartPullRefresh(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
